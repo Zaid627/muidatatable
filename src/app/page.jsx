a2029987e5ccc053a9813fb5545e3d821c2ed00b
zaid <muhammadzaid627@gmail.com>
2024-10-15 13:37:52 +0500

    changing

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Posts from "./pages/posts";
+import { Tooltip } from "@mui/material";
 
 
 
 export default function Home() {
   return (
-    <Posts/>
+    <Tooltip>
+     <Posts/>
+
+    </Tooltip>
+    
   );
 }
